a730c96b820bdde48f09564c0dca22d42e7c3918
Stefan Contiu <stefan.contiu@gmail.com>
2015-06-26 00:13:55 +0300

    Added rest of spectral bands to data set

--- preproc/BuildFeatures.m
+++ preproc/BuildFeatures.m
@@ -1,13 +1,15 @@
+LandsatImageName = 'f:\edu\Research\dataset\Test_Misuri\landsat\LC80230342014186LGN01_';
+
 start_crop_x = 6900;
 start_crop_y = 4200;
 
 cr = {[6900,7100],[4200,4400]};
 % Red
-B4 = imread('f:\edu\Research\dataset\Test_Misuri\landsat\LC80230342014186LGN01_B4.TIF', 'PixelRegion', cr);
+B4 = imread(strcat(LandsatImageName, 'B4.TIF'), 'PixelRegion', cr);
 % Green
-B3 = imread('f:\edu\Research\dataset\Test_Misuri\landsat\LC80230342014186LGN01_B3.TIF', 'PixelRegion', cr);
+B3 = imread(strcat(LandsatImageName, 'B3.TIF'), 'PixelRegion', cr);
 % Blue
-B2 = imread('f:\edu\Research\dataset\Test_Misuri\landsat\LC80230342014186LGN01_B2.TIF', 'PixelRegion', cr);
+B2 = imread(strcat(LandsatImageName, 'B2.TIF'), 'PixelRegion', cr);
 
 rgbImage = cat(3, B4, B3, B2);
 imshow(rgbImage);
@@ -15,20 +17,26 @@ imshow(rgbImage);
 %NIR
 B5 = imread('f:\edu\Research\dataset\Test_Misuri\landsat\LC80230342014186LGN01_B5.TIF', 'PixelRegion', cr);
 
-
 % Compute NDVI
 NDVI = double(B5 - B4) ./ double(B5 + B4);
 imshow(NDVI, 'DisplayRange', [-1 1]);
 
+% Load rest of the spectral bands, note: skip Band 8 since LANDSAT8 has an
+% increased resolution for it
+B1 = imread(strcat(LandsatImageName, 'B1.TIF'), 'PixelRegion', cr);
+B5 = imread(strcat(LandsatImageName, 'B5.TIF'), 'PixelRegion', cr);
+B6 = imread(strcat(LandsatImageName, 'B6.TIF'), 'PixelRegion', cr);
+B7 = imread(strcat(LandsatImageName, 'B7.TIF'), 'PixelRegion', cr);
+B9 = imread(strcat(LandsatImageName, 'B9.TIF'), 'PixelRegion', cr);
 
+% Load the ground truth image and compute the data set
 GT = imread('f:\edu\Research\dataset\Test_Misuri\ground_truth_v1_16colors.png');
-
 size(GT)
 yellow = 0;
 blue = 0;
 red = 0;
 green = 0;
-dataset = [0.1 0.2 0.3 0.4 0];
+dataset = [0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 0];
 for row=1:size(GT,1) 
     for col=1:size(GT,2)
         class = 0;
@@ -52,15 +60,17 @@ for row=1:size(GT,1)
         if (class > 0)
             % we have a new data set entry
             double(NDVI(row, col));
-            entry = [double(B2(row, col)) double(B3(row, col)) double(B4(row, col)) double(NDVI(row, col)) double(class)];
-            size(dataset)
-            size(entry)
+            entry = [double(B1(row, col)) double(B2(row, col)) ...
+                double(B3(row, col)) double(B4(row, col)) ...
+                double(B5(row, col)) double(B6(row, col)) ...
+                double(B7(row, col)) double(B9(row, col)) ...
+                double(NDVI(row, col)) double(class)];
             dataset = [dataset; entry];
         end
     end
 end
 size(dataset)
-csvwrite('f:\edu\Research\dataset\Test_Misuri\dataset_v1.csv', dataset);
+csvwrite('f:\edu\Research\dataset\Test_Misuri\dataset_v2.csv', dataset);
 %yellow 
 %blue
 %red
